af1dd77463f6ce73412d2d5babc2e387fe8fe5d8
Li Song <iseansay@gmail.com>
2018-05-11 10:36:52 +0800

    hotfix parse img

--- app/src/parse.js
+++ app/src/parse.js
@@ -3,7 +3,8 @@ import wtf from 'wtf_wikipedia'
 const parse = (title, lang = 'en') => {
   return wtf.fetch(title, lang).then((doc) => {
       const description = doc.sections()[0] && doc.sections()[0].data.sentences.map(sentence => sentence.text).join(' ')
-      const image = doc.images(0).src()
+      const img = doc.images(0)
+      const image = img && img.src()
       return {
         description,
         title,
